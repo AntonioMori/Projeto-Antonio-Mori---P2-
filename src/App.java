53375a029e39037e1060663e92163babdb995f06
Antonio Mori <camargomoritrancoso@hotmail.com>
2022-11-30 23:16:20 -0300

    Tela de loading concluida

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    // Aqui é um código que a cada letra ele vai escrevendo na tela com o delay personalizado, criando um efeito de delay
+    // Aqui é um código que a cada letra ele vai escrevendo na tela com atraso personalizado, criando um efeito de delay
     public static void slowestPrint(String output) {
         for (int i = 0; i<output.length(); i++) {
           char c = output.charAt(i);
@@ -31,7 +31,7 @@ public class App {
     }
     public static void main(String[] args) throws Exception {
     
-        //slowPrint("oi esse é um teste para ver se está funcionando");
+
         // Scanner definido como input(entrada)
         Scanner input = new Scanner(System.in);
 
@@ -76,23 +76,40 @@ public class App {
         Thread.sleep(2000);
         slowestPrint("[end of transmition] \n\nDigite \"yes\" para iniciar modo de segurança.");
 
-
+        // Variável que recebe a resposta do usuário, levando a um dos finais caso a resposta seja "yes" ou "no"
 
         String resposta = input.nextLine();
+
         if (resposta.equals("yes")) {
+            //se a resposta for sim:
+
+            slowestPrint("Modo de segurança sendo iniciado...");
+            Thread.sleep(1000);
+            //Criação da variável fora do for para que ela não seja recriada a cada loop
+            // início da tela de loading
             
-            System.out.println("Modo de segurança iniciado.");
-            System.out.println("");
-         
-            System.out.println("Seja bem vindo á parte fácil! A construção do seu guerreiro, escolha as peças com sabedoria ou encare a derrota iminente.");
-            System.out.println("Qual é o seu nome e o do seu robozinho? \"ps: separe os nomes atraves de uma vírgula\"");
+            int porcentagem = 5;
+            for (int i = 0; i < 20; i++) { 
+                System.out.println( porcentagem + "%");
+                Thread.sleep(500);
+                porcentagem += 5;
+            }
+                
+                slowestPrint("\nModo de segurança iniciado com sucesso.");
+                slowestPrint("[Sistema auxiliar ligado]");
+            // continuação da história e início das escolhas 
+
+            
+            Thread.sleep(2000);
+            slowPrint("\n\nSeja bem vindo ao menu interativo! Eu irei auxiliar na construção do seu robo guerrilheiro, escolha as peças com sabedoria ou encare a derrota iminente.");
+            slowPrint("\nQual é o seu nome e o do seu robozinho? \"ps: separe os nomes atraves de uma vírgula\"");
             //use a ferramenta split para separar usando uma virgula , leia  a linha inteira usando nextLine e depois use o split
             
             String nomeInteiro = input.nextLine();
             String[] nomes = nomeInteiro.split(",");
             String nomeUser = nomes[0];
             String nomeRobo = nomes[1];
-            System.out.println("\nPrazer "+nomeUser+"! Seu robozinho se chamará "+nomeRobo+"!\n\nAgora vamos dar continuidade a construção do seu robo!\nVocê deseja que ele seja feito de qual material?\n[Aço] / [Ouro] / [Cobre] / [Plástico]");
+            slowPrint("\nPrazer "+nomeUser+"! Seu robozinho se chamará "+nomeRobo+"!\n\nAgora vamos dar continuidade a construção!\nVocê deseja que ele seja feito de qual material?\n[Aço] / [Ouro] / [Cobre] / [Plástico]");
             String tipoRobo = input.nextLine();
             while(!tipoRobo.equalsIgnoreCase("Aço") && !tipoRobo.equalsIgnoreCase("Ouro") && !tipoRobo.equalsIgnoreCase("Cobre") && !tipoRobo.equalsIgnoreCase("Plástico")){
                 System.out.println("Você digitou algo errado, tente novamente!\nVocê deseja que ele seja feito de qual material?\n[Aço] / [Ouro] / [Cobre] / [Plástico]");
@@ -118,7 +135,9 @@ public class App {
         
         } 
         else {
-            System.out.println("Modo de segurança não iniciado.");
+            slowestPrint("Modo de segurança não iniciado.");
+            slowestPrint("\nAs instruções foram ignoradas. O cientista e sua familia morreram.");
+            slowestPrint("\n\n[GAME OVER]");
             
         }
         
---
+++ src/testes.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class testes {
+    
+    public static void main(String[] args) throws Exception {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("[end of transmition] \n\nDigite \"yes\" para iniciar modo de segurança.");
+
+        // Variável que recebe a resposta do usuário
+
+        String resposta = input.nextLine();
+        if (resposta.equals("yes")) {
+            System.out.println("Modo de segurança sendo iniciado...");
+            Thread.sleep(1000);
+            int porcentagem = 0;
+            for (int i = 0; i < 11; i++) {
+                System.out.println( porcentagem + "%");
+                Thread.sleep(500);
+                porcentagem = porcentagem + 10;
+            }
+            System.out.println("\nModo de segurança iniciado com sucesso.");
+            System.out.println("[Sistema auxiliar ligado]");
+         
+            System.out.println("Seja bem vindo á parte fácil! A construção do seu guerreiro, escolha as peças com sabedoria ou encare a derrota iminente.");
+            System.out.println("Qual é o seu nome e o do seu robozinho? \"ps: separe os nomes atraves de uma vírgula\"");
+
+
+
+    
+}}}
