91d731e5a510459ee84b145184472924aebf613f
Antonio Mori <camargomoritrancoso@hotmail.com>
2022-12-24 01:43:25 -0300

    24 12 2022

---
+++ src/Caps.java
@@ -0,0 +1,22 @@
+public class Caps {
+    
+
+
+ capitulo(String nome, String textoHistoria, String op1, String op2){
+nome =this.nome;
+textoHistoria = this.textoHistoria;
+op1 = this.op1;
+op2 = this.op2;
+
+
+
+
+}
+
+
+
+
+
+
+
+}
